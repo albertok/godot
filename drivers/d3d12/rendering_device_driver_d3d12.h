d08c6c3a465e69a880d1957f0634df28f5bd2eb1
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2025-11-20 19:26:00 +0300

    Check for Typed UAV Load Additional Formats capability when creating D3D12 device.
    (cherry picked from commit 23314701285b9e2011d365428e7f452be9d103bc)

--- drivers/d3d12/rendering_device_driver_d3d12.h
+++ drivers/d3d12/rendering_device_driver_d3d12.h
@@ -114,10 +114,6 @@ class RenderingDeviceDriverD3D12 : public RenderingDeviceDriver {
 		bool native_16bit_ops = false;
 	};
 
-	struct StorageBufferCapabilities {
-		bool storage_buffer_16_bit_access_is_supported = false;
-	};
-
 	struct FormatCapabilities {
 		bool relaxed_casting_supported = false;
 	};
@@ -143,7 +139,6 @@ class RenderingDeviceDriverD3D12 : public RenderingDeviceDriver {
 	FragmentShadingRateCapabilities fsr_capabilities;
 	FragmentDensityMapCapabilities fdm_capabilities;
 	ShaderCapabilities shader_capabilities;
-	StorageBufferCapabilities storage_buffer_capabilities;
 	FormatCapabilities format_capabilities;
 	BarrierCapabilities barrier_capabilities;
 	MiscFeaturesSupport misc_features_support;
